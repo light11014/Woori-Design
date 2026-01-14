0b24647e94b787e3362ca6141c385cbc1199b4a5
seonjiwon <tommys915@gmail.com>
2026-01-14 16:50:29 +0900

    feature: fix

--- src/data/cards.mock.js
+++ src/data/cards.mock.js
@@ -1,50 +1,57 @@
 export const mockCards = [
-  {
-    cardId: "card-001",
-    cardName: "K-패스 우리카드",
-    company: "우리카드",
-    cardType: "CREDIT",
+    {
+        cardId: "card-001",
+        cardName: "K-패스 우리카드",
+        company: "우리카드",
+        cardType: "CREDIT",
 
-    imageUrl: "/woori-card-kpass.png",
-    themeColor: "#1A4FFF",
+        imageUrl: "/woori-card-kpass.png",
+        themeColor: "#1A4FFF",
 
-    maskedCardNumber: "**** **** **** 1234",
+        maskedCardNumber: "**** **** **** 1234",
 
-    linkedAccount: {
-      bankName: "신한",
-      maskedAccountNumber: "110-***-**233",
+        linkedAccount: {
+            bankName: "신한",
+            maskedAccountNumber: "110-***-**233",
+        },
+
+        payon: true,
+        overseasPayment: "master",
     },
-  },
-  {
-    cardId: "card-002",
-    cardName: "KREAM Platinum 우리카드",
-    company: "우리카드",
-    cardType: "CREDIT",
+    {
+        cardId: "card-002",
+        cardName: "KREAM Platinum 우리카드",
+        company: "우리카드",
+        cardType: "CREDIT",
 
-    imageUrl: "/woori-card-kream-platinum.png",
-    themeColor: "#2B2B2B",
+        imageUrl: "/woori-card-kream-platinum.png",
+        themeColor: "#2B2B2B",
 
-    maskedCardNumber: "**** **** **** 5678",
+        maskedCardNumber: "**** **** **** 5678",
 
-    linkedAccount: {
-      bankName: "국민",
-      maskedAccountNumber: "012-***-**435",
+        linkedAccount: {
+            bankName: "국민",
+            maskedAccountNumber: "012-***-**435",
+        },
+        payon: false,
+        overseasPayment: "visa",
     },
-  },
-  {
-    cardId: "card-003",
-    cardName: "카드의정석 오하CHECK",
-    company: "우리카드",
-    cardType: "CHECK",
+    {
+        cardId: "card-003",
+        cardName: "카드의정석 오하CHECK",
+        company: "우리카드",
+        cardType: "CHECK",
 
-    imageUrl: "/woori-card-standard.png",
-    themeColor: "#2B2B2B",
+        imageUrl: "/woori-card-standard.png",
+        themeColor: "#2B2B2B",
 
-    maskedCardNumber: "**** **** **** 2456",
+        maskedCardNumber: "**** **** **** 2456",
 
-    linkedAccount: {
-      bankName: "우리",
-      maskedAccountNumber: "1002-***-**314",
+        linkedAccount: {
+            bankName: "우리",
+            maskedAccountNumber: "1002-***-**314",
+        },
+        payon: true,
+        overseasPayment: "visa",
     },
-  },
 ];
